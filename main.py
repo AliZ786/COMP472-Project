f92ae8e787c5af08e8d06145caa25c712b6c3d80
Laila Alhalabi <laila.alhalabi99@gmail.com>
2022-06-04 16:32:11 -0400

    data processing and loading updated

--- main.py
+++ main.py
@@ -6,22 +6,18 @@ import torch.nn.functional as F
 import torch.utils.data as td
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import ToTensor
+import torchvision.datasets
 
 # Install PyTorch first
-
 # To download torch, create the following environment using Anaconda:
 'conda create -n pytorch python=3.6'
-
 # Then activate the environment by running
 'conda activate pytorch'
-
 # Install using pip torch & torch vision
 'pip install torchvision --user'
-
 # Open python shell and import the following
 'import torch'
 'import torchvision'
-
 # Then run this app using anaconda
 'python main.py'
 
@@ -34,30 +30,23 @@ print('Images Collected Statistics:')
 # dataset
 data_dir = './dataset'
 dataset = ImageFolder(data_dir, transform=ToTensor())
-print("- The dataset has 2 classes", dataset.classes,
+print("- The dataset has classes", dataset.classes,
       "and contains", len(dataset), "images")
 
-# Training data
-training_dir = './dataset/Training'
-training_dataset = ImageFolder(training_dir, transform=ToTensor())
-print("- The training dataset has the classes", training_dataset.classes,
-      "and contains", len(training_dataset), "images")
+num_epochs = 4
+num_classes = 4
+learning_rate = 0.001
 
-# Testing data
-testing_dir = './dataset/Testing'
-testing_dataset = ImageFolder(testing_dir, transform=ToTensor())
-print("- The testing dataset has the classes", testing_dataset.classes,
-      "and contains", len(testing_dataset), "images")
+# transform process the images (resizing and normalizing)
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-# Processing the images (resizing and normalizing)
-dataset = ImageFolder(data_dir, transforms.Compose([
-    transforms.Resize((32, 32)),
-    transforms.ToTensor(),
-    transforms.Normalize(*([0.485, 0.456, 0.406], [0.225, 0.225, 0.225]))
-]))
+# loading the images using DataLoader
+trainset = torchvision.datasets.ImageFolder(root='./dataset', transform=transform)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=300, shuffle=True, num_workers=2)
 
-# Loading the images using DataLoader
-train_loader = torch.utils.data.DataLoader(
-    training_dir, batch_size=300, shuffle=True, pin_memory=True)
-test_loader = torch.utils.data.DataLoader(
-    testing_dir, batch_size=100, pin_memory=True)
+testset = torchvision.datasets.ImageFolder(root='./dataset', transform=transform)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+# defining classes
+classes = ('No mask', 'N95', 'Surgical', 'Cloth')
+
