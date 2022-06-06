bd62f4c7787ac739b15c2f20bb6d76b9980ff670
Laila Alhalabi <laila.alhalabi99@gmail.com>
2022-06-06 18:34:43 -0400

    CNN update

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+
+from PIL import Image
+
 if __name__ == '__main__':
       import torch
       import torchvision.transforms as transforms
@@ -7,7 +11,6 @@ if __name__ == '__main__':
       import torch.utils.data as td
       from torchvision.datasets import ImageFolder
       from torchvision.transforms import ToTensor
-      import torchvision.datasets
 
       # Install PyTorch first
       # To download torch, create the following environment using Anaconda:
@@ -29,8 +32,7 @@ if __name__ == '__main__':
       print('Images Collected Statistics:')
 
       # dataset
-      data_dir = './dataset'
-      dataset = ImageFolder(data_dir, transform=ToTensor())
+      dataset = ImageFolder('./dataset', transform=ToTensor())
       print("- The dataset has classes", dataset.classes,
             "and contains", len(dataset), "images")
 
@@ -40,84 +42,67 @@ if __name__ == '__main__':
 
       # transform process the images (resizing and normalizing)
       transform= transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.RandomHorizontalFlip(),  # randomly flip and rotate
-            transforms.RandomRotation(10),
+            transforms.Resize((64, 64)),
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-      # loading the images using DataLoader
-      train_len = len(dataset) - 100
-      test_len = 100
-      trainset, testset = torch.utils.data.random_split(dataset, [train_len, test_len])
+      training_set = datasets.ImageFolder(root='./dataset/Training', transform=transform)
+      train_loader = torch.utils.data.DataLoader(training_set, batch_size=4, shuffle=True, num_workers=2)
+      print("- The training dataset has classes", training_set.classes,
+            "and contains", len(training_set), "images")
 
-      trainset = datasets.ImageFolder(data_dir, transform=transform)
-      train_loader = torch.utils.data.DataLoader(trainset, batch_size=40, shuffle=True, num_workers=2)
+      testing_set = datasets.ImageFolder(root='./dataset/Testing', transform=transform)
+      test_loader = torch.utils.data.DataLoader(testing_set, batch_size=4, shuffle=False, num_workers=2)
+      print("- The testing dataset has classes", testing_set.classes,
+            "and contains", len(testing_set), "images")
 
-      testset = datasets.ImageFolder(data_dir, transform=transform)
-      test_loader = torch.utils.data.DataLoader(testset, batch_size=40, shuffle=False, num_workers=2)
+      device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+      print("The device used is", device)
 
-      device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+      classes = ('No Mask', 'Cloth Mask', 'N95 Mask', 'Surgical Mask')
 
       class CNN(nn.Module):
             def __init__(self):
                   super(CNN, self).__init__()
-                  self.conv_layer = nn.Sequential(
-                        nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
-                        nn.BatchNorm2d(32),
-                        nn.LeakyReLU(inplace=True),
-                        nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
-                        nn.BatchNorm2d(32),
-                        nn.LeakyReLU(inplace=True),
-                        nn.MaxPool2d(kernel_size=2, stride=2),
-                        nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
-                        nn.BatchNorm2d(64),
-                        nn.LeakyReLU(inplace=True),
-                        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-                        nn.BatchNorm2d(64),
-                        nn.LeakyReLU(inplace=True),
-                        nn.MaxPool2d(kernel_size=2, stride=2),
-                  )
-                  self.fc_layer = nn.Sequential(
-                        nn.Dropout(p=0.1),
-                        nn.Linear(8 * 8 * 64, 1000),
-                        nn.ReLU(inplace=True),
-                        nn.Linear(1000, 512),
-                        nn.ReLU(inplace=True),
-                        nn.Dropout(p=0.1),
-                        nn.Linear(512, 10)
-                  )
+                  self.conv1 = nn.Conv2d(3, 6, 5)
+                  self.pool = nn.MaxPool2d(2, 2)
+                  self.conv2 = nn.Conv2d(6, 16, 5)
+                  self.fc1 = nn.Linear(16 * 13 * 13, 120)
+                  self.fc2 = nn.Linear(120, 84)
+                  self.fc3 = nn.Linear(84, 10)
 
             def forward(self, x):
-                  # conv layers
-                  x = self.conv_layer(x)
-                  # flatten
-                  x = x.view(x.size(0), -1)
-                  # fc layer
-                  x = self.fc_layer(x)
+                  x = self.pool(F.relu(self.conv1(x)))
+                  x = self.pool(F.relu(self.conv2(x)))
+                  x = x.view(4, 16 * 13 * 13)
+                  x = F.relu(self.fc1(x))
+                  x = F.relu(self.fc2(x))
+                  x = self.fc3(x)
                   return x
 
-
-      model = CNN()
+      model = CNN().to(device)
 
       criterion = nn.CrossEntropyLoss()
-      optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+      optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-      total_step = len(trainset)
+      total_step = len(train_loader)
       loss_list = []
       acc_list = []
 
       for epoch in range(num_epochs):
-            for i, (images, labels) in enumerate(trainset):
+            for i, (images, labels) in enumerate(train_loader, 0):
+
+                  images, labels = images.to(device), labels.to(device)
+
                   # Forward pass
                   outputs = model(images)
                   loss = criterion(outputs, labels)
                   loss_list.append(loss.item())
+
                   # Backprop and optimisation
-                  optimizer.zero_grad()
                   loss.backward()
                   optimizer.step()
+
                   # Train accuracy
                   total = labels.size(0)
                   _, predicted = torch.max(outputs.data, 1)
@@ -127,7 +112,7 @@ if __name__ == '__main__':
                         print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
                               .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(), (correct / total) * 100))
 
-      model.eval()
+      print('Training Done')
 
       # with torch.no_grad():
       #       correct = 0
